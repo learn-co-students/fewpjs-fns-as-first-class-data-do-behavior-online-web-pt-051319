6a5f4c97bb700f4b135fb01137d29d10c6c27137
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-20 22:41:14 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time){
+  let time_array = time.split(":");
+  let hh = parseInt(time_array[0]);
+  let mm = parseInt(time_array[1]);
+  if (hh <= 12){
+    return "Good Morning";
+  }
+  if (12 < hh && hh < 17) {
+    return "Good Afternoon";
+  }
+  if (17 < hh) {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(string){
+  let greeting = document.getElementById('greeting')
+      greeting.innerText = `${string}`;
+}
