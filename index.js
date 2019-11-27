71c7b80bb68156214e6e76c45829a8de6e571814
Jess Rezac <jess.rezac@gmail.com>
2019-11-27 16:01:36 -0600

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const timeInt = parseInt(time.split(":", 1));
+  if ( timeInt < 12 )  {
+    return `Good Morning`;
+  } else if ( timeInt > 17 ) {
+    return `Good Evening`;
+  } else {
+    return `Good Afternoon`;
+  } 
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg;
+}
